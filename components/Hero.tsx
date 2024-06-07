75ccd42fdf131ec2e28a72f197b3e96a90580c1b
Kevin <kevin.shehan30@gmail.com>
2024-06-08 01:05:06 +0530

    Hero H2 Added

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -27,10 +27,14 @@ const Hero = () => {
       >
         <div
           className="absolute pointer-events-none
-       inset-0 flex items-center justify-center
-        dark:bg-black-100 bg-white 
+       inset-0 flex items-center justify-center dark:bg-black-100 bg-white 
         [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"
         />
+      </div>
+      <div className="flex justify-center">
+        <div className="max-w-[89vw]">
+          <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">Dynamic Web Magic with Next.js</h2>
+        </div>
       </div>
     </div>
   );
